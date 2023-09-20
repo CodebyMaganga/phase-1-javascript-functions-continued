e6339ab33ed406c6fd65428e4b42edf71d6fd0b1
Maganga Mwambonu <codebymaganga@gmail.com>
2023-09-20 20:11:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(defaultActivity = 'roller-skate'){
+    return `This Saturday, I want to ${defaultActivity}!`
+}
+console.log(saturdayFun("bathe my dog"))
+
+function mondayWork(defaultActivity = 'go to the office'){
+    return `This Monday, I will ${defaultActivity}.`
+}
+console.log(mondayWork())
+
+function wrapAdjective(visualflair = "*"){
+    return function innerPart(defaultName = "Special"){
+            return `You are ${visualflair}${defaultName}${visualflair}!`
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
+
+console.log(wrapAdjective("%")("a dedicated programmer"))
